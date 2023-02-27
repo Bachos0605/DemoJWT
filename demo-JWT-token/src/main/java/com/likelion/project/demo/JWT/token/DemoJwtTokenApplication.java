b492441b933a3418d7558acfd56a1cc65efbc08b
Bachos0605 <93861119+Bachos0605@users.noreply.github.com>
2023-02-27 23:55:50 +0700

    Add files via upload

---
+++ demo-JWT-token/src/main/java/com/likelion/project/demo/JWT/token/DemoJwtTokenApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.project.demo.JWT.token;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJwtTokenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJwtTokenApplication.class, args);
+	}
+
+}
